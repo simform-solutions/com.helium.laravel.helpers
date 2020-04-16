62cab75bfa80892b0b77813e00ebcc1f04e41ae9
Nick Rupert <nick@heliumservices.com>
2020-04-16 08:35:52 -0400

    Added BaseTraits

--- src/Traits/BaseTraits.php
+++ src/Traits/BaseTraits.php
@@ -2,6 +2,13 @@
 
 namespace Helium\LaravelHelpers\Traits;
 
+use Czim\NestedModelUpdater\Traits\NestedUpdatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 trait BaseTraits
 {
+	use GeneratesPrimaryKey;
+	use NestedUpdatable;
+	use SelfValidates;
+	use SoftDeletes;
 }
