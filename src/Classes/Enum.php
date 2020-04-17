0f26cba36fae409b6d1097053235826b0fd26b96
Nick Rupert <nick@heliumservices.com>
2020-04-17 15:15:44 -0400

    Implemented basic Enum

--- src/Classes/Enum.php
+++ src/Classes/Enum.php
@@ -2,10 +2,21 @@
 
 namespace Helium\LaravelHelpers\Classes;
 
+use ReflectionClass;
+
 abstract class Enum
 {
-	public static function all(bool $hasNull = false): array
+	public static function all(bool $acceptNull = false): array
 	{
-		return [];
+		$reflector = new ReflectionClass(static::class);
+
+		$all = array_values($reflector->getConstants());
+
+		if ($acceptNull)
+		{
+			$all[] = null;
+		}
+
+		return $all;
 	}
 }
