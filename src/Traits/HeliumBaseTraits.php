5d798f04a3013bb9975e8b0e4a688597c2b93d4e
Nick Rupert <nick@heliumservices.com>
2020-04-17 12:53:39 -0400

    Added SoftCascadeTrait

--- src/Traits/HeliumBaseTraits.php
+++ src/Traits/HeliumBaseTraits.php
@@ -2,13 +2,15 @@
 
 namespace Helium\LaravelHelpers\Traits;
 
+use Askedio\SoftCascade\Traits\SoftCascadeTrait;
 use Czim\NestedModelUpdater\Traits\NestedUpdatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-trait BaseTraits
+trait HeliumBaseTraits
 {
 	use GeneratesPrimaryKey;
 	use NestedUpdatable;
 	use SelfValidates;
 	use SoftDeletes;
+	use SoftCascadeTrait;
 }
