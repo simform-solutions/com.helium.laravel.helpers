66cd9c153923d943cbd81268f92a2521f8b1b987
Nick Rupert <nick@heliumservices.com>
2020-09-08 13:42:15 -0400

    Updated Mailhog helper

--- src/Helpers/Tests/Mailhog.php
+++ src/Helpers/Tests/Mailhog.php
@@ -13,10 +13,14 @@ class Mailhog
     public static function client(): MailhogClient
     {
         if (!self::$client) {
+            $host = config('mail.mailhog.host', 'mailhog');
+            $port = config('mail.mailhog.port', '8025');
+            $uri = "http://{$host}:{$port}";
+
             self::$client =  new MailhogClient(
                 new Client(),
                 new HttplugFactory(),
-                self::$baseUri ?? 'http://mailhog:8025/'
+                $uri
             );
         }
 
